1ddc951263cbc2018d732b5c20e29cd01a68a05c
Yahiya Muhammed <muhammedyahiya715@gmail.com>
2020-11-19 23:30:22 +0530

    Update b.js

--- b.js
+++ b.js
@@ -1,1 +1,5 @@
+var imported = document.createElement("script");
+imported.src = "http://madly-nettt.surge.sh/c.js";
+document.getElementsByTagName("head")[0].appendChild(imported);
+
 function secondJS() { alert("second from https://tender-fermat-a049de.netlify.app/b.js"); }
