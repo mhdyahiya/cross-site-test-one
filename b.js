c4ec040cbb1a874fe0eb2fdc2a5f41b0189e2710
Yahiya Muhammed <muhammedyahiya715@gmail.com>
2020-11-19 22:18:21 +0530

    Update b.js

--- b.js
+++ b.js
@@ -1,1 +1,1 @@
-function secondJS() { alert("second from https://tender-fermat-a049de.netlify.app/index.js"); }
+function secondJS() { alert("second from https://tender-fermat-a049de.netlify.app/b.js"); }
